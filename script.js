f7670cec62179cda8d1eb869e498749ff6f87c0d
OliverModiba <52031303+OliverModiba@users.noreply.github.com>
2025-12-24 14:11:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 
+const roles = [
+  "Senior Data Analyst",
+  "Audit & Risk Analytics Specialist",
+  "Automation & BI Professional"
+];
+
+let roleIndex = 0;
+let charIndex = 0;
+const typingEl = document.querySelector(".typing");
+
+function type() {
+  if (charIndex < roles[roleIndex].length) {
+    typingEl.textContent += roles[roleIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, 80);
+  } else {
+    setTimeout(erase, 2000);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    typingEl.textContent = roles[roleIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, 40);
+  } else {
+    roleIndex = (roleIndex + 1) % roles.length;
+    setTimeout(type, 600);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", type);
